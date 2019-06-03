cc06738c3db887047a07c6be6f5706c0b061f78c
jananthan93 <jjananthan93@gmail.com>
2019-06-03 10:27:43 +0530

    leave request reducer import index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 import CollapseSideBarReducer from './reducer-aside-toggle';
 import FetchCancelLeaveRequestsReducer from './reducer-fetchCRL'
 import userLoginReducer from './reducer-user-login';
-
+import FetchLeaveRequestsReducer from './leave-reducer'
 
 
 const allReducers = combineReducers({
